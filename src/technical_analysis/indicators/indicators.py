7be27ad74ef28ae1deaa27b38e7c9a3321a54f78
trevormcguire <trevormcguire13@gmail.com>
2022-10-18 19:23:34 -0400

    add stochastic, trix, tsi

--- src/technical_analysis/indicators/indicators.py
+++ src/technical_analysis/indicators/indicators.py
@@ -1,8 +1,9 @@
+from typing import Tuple
+
 import numpy as np
 import pandas as pd
 
 from technical_analysis.overlays.moving_average import sma, ema
-
 
 
 def std(price: pd.Series, period: int) -> pd.Series:
@@ -119,3 +120,68 @@ def tsi(price: pd.Series, period1: int = 25, period2: int = 13) -> pd.Series:
     pc = np.abs(pc)
     double_smoothed_abs_pc = ema(ema(pc, period1), period2)
     return 100 * (double_smoothed_pc / double_smoothed_abs_pc)
+
+
+def trix(price: pd.Series, period: int = 15) -> pd.Series:
+    """
+    Displays percent rate of change as a triply smoothed moving average
+        > similar to MACD, but smoother
+
+    Calculation:
+    ------------
+        1. Single-Smoothed EMA = 15-period EMA of the closing price
+        2. Double-Smoothed EMA = 15-period EMA of Single-Smoothed EMA
+        3. Triple-Smoothed EMA = 15-period EMA of Double-Smoothed EMA
+        4. TRIX = 1-period percent change in Triple-Smoothed EMA
+    
+    Reference:
+    -----------
+        https://school.stockcharts.com/doku.php?id=technical_indicators:trix
+    """
+    trix = ema(ema(ema(price, period), period), period)
+    return trix.pct_change(1)
+
+
+def stochastic(high: pd.Series,
+               low: pd.Series,
+               close: pd.Series,
+               period: int,
+               perc_k_smoothing: int = 0,
+               perc_d_smoothing: int = 3) -> Tuple[pd.Series]:
+    """
+    Stochastic Oscillator
+    ----------
+    
+    Calculation:
+    ----------
+        %K = (Current Close - Lowest Low)/(Highest High - Lowest Low) * 100
+        %D = 3-day SMA of %K
+    
+    Three modes:
+    ----------
+        1. Fast
+            perc_k_smoothing = 0
+            perc_d_smoothing = 3
+
+        2. Slow
+            perc_k_smoothing = 3
+            perc_d_smoothing = 3
+
+        3. Full
+            perc_k_smoothing > 3
+            perc_d_smoothing > 3
+
+    Reference:
+    -----------
+        https://school.stockcharts.com/doku.php?id=technical_indicators:stochastic_oscillator_fast_slow_and_full
+
+    """
+    lowest_low = low.rolling(period).min()
+    highest_high = high.rolling(period).max()
+
+    perc_k = 100 * ((close - lowest_low)/(highest_high - lowest_low))
+    if perc_k_smoothing:
+        perc_k = sma(perc_k, perc_k_smoothing)
+    perc_d = sma(perc_k, perc_d_smoothing)  # the trigger line
+    return perc_k, perc_d
+
