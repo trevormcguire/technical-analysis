0af258741aae35c912471d5ae093001e81024e91
Trevor McGuire <trevor@Trevors-MBP.lan>
2025-03-12 10:28:18 -0400

    add tests for new indicators

--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -37,3 +38,22 @@ def rvol_results() -> pd.Series:
 def test_rvol(input_df, rvol_results):
     result = indicators.rvol(input_df["volume"], period=6).dropna().reset_index(drop=True).round(2)
     assert (result == rvol_results).all()
+
+@pytest.fixture
+def money_flow_volume_results() -> pd.Series:
+    # [(Close  -  Low) - (High - Close)] /(High - Low)
+    return pd.Series([0.333333 * 100, 0.0 * 200, 0.0 * 300, 0.333333 * 400, 0.5 * 500, -1.0 * 600])
+
+def test_money_flow_volume(input_df, money_flow_volume_results):
+    result = indicators.money_flow_volume(input_df["high"], input_df["low"], input_df["close"], input_df["volume"])
+    result = result.round(4)
+    assert np.isclose(result, money_flow_volume_results).all()
+
+
+@pytest.fixture
+def on_balance_volume_results():
+    return pd.Series(np.cumsum([-200, 300, 400, -500, -600]))
+
+def test_on_balance_volume(input_df, on_balance_volume_results):
+    result = indicators.on_balance_volume(input_df["close"], volume=input_df["volume"]).dropna().reset_index(drop=True)
+    assert (result == on_balance_volume_results).all()
